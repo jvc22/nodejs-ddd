c0455b7c61cb19a56b9cf582fdab38c17026a602
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-10 21:27:13 -0300

    feat: install fastify-jwt module and implement middleware

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,14 +1,23 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import {
   type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { env } from './env'
 import { sharersRoutes } from './http/controllers/sharers/_routes'
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: '3d',
+  },
+})
 
 app.register(sharersRoutes)
--- src/infra/env.ts
+++ src/infra/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['development', 'test', 'production']),
   DATABASE_URL: z.string(),
+  JWT_SECRET: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
