88bbd65e13591510e96557a1474248d40c5a9f8f
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-11 18:35:51 -0300

    test: implement edit-profile controller e2e test

---
+++ src/infra/http/controllers/sharers/edit-profile.spec.ts
@@ -0,0 +1,29 @@
+import { app } from '@/infra/app'
+import request from 'supertest'
+import { createAndAuthenticateSharer } from 'test/utils/create-and-authenticate-sharer'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Edit Profile (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should edit a sharer profile', async () => {
+    const newName = 'John Doe'
+
+    const { token } = await createAndAuthenticateSharer(app)
+
+    const response = await request(app.server)
+      .get('/sharers')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        newName,
+      })
+
+    expect(response.statusCode).toBe(200)
+  })
+})
