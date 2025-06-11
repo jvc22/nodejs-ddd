e3f70d36407fd9f28c703f0ce0f291a01965d2c4
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-11 18:49:05 -0300

    test: implement register-link controller e2e test

---
+++ src/infra/http/controllers/links/register-link.spec.ts
@@ -0,0 +1,29 @@
+import { app } from '@/infra/app'
+import { faker } from '@faker-js/faker'
+import request from 'supertest'
+import { createAndAuthenticateSharer } from 'test/utils/create-and-authenticate-sharer'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register Link (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should register a link', async () => {
+    const { token } = await createAndAuthenticateSharer(app)
+
+    const response = await request(app.server)
+      .post('/links')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: faker.company.name(),
+        url: faker.internet.url(),
+      })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
