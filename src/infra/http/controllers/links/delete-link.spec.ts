ba0dd6af910af32483c1728f38f39953b494814a
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-12 08:47:22 -0300

    test: implement delete-link controller e2e test

---
+++ src/infra/http/controllers/links/delete-link.spec.ts
@@ -0,0 +1,59 @@
+import { app } from '@/infra/app'
+import { faker } from '@faker-js/faker'
+import request from 'supertest'
+import { createAndAuthenticateSharer } from 'test/utils/create-and-authenticate-sharer'
+import { createLink } from 'test/utils/create-link'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Delete Link (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should delete a link', async () => {
+    const { token } = await createAndAuthenticateSharer(app)
+    const { link } = await createLink(app, token)
+
+    const response = await request(app.server)
+      .delete(`/links/${link.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  it('should not delete a link with wrong id', async () => {
+    const { token } = await createAndAuthenticateSharer(app)
+
+    const response = await request(app.server)
+      .delete(`/links/${faker.string.uuid()}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toBe(404)
+    expect(response.body).toMatchObject({
+      message: expect.any(String),
+    })
+  })
+
+  it('should not delete a link from another sharer', async () => {
+    const { token: firstUserToken } = await createAndAuthenticateSharer(app)
+    const { link } = await createLink(app, firstUserToken)
+
+    const { token: secondUserToken } = await createAndAuthenticateSharer(app)
+
+    const response = await request(app.server)
+      .delete(`/links/${link.id}`)
+      .set('Authorization', `Bearer ${secondUserToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(401)
+    expect(response.body).toMatchObject({
+      message: expect.any(String),
+    })
+  })
+})
